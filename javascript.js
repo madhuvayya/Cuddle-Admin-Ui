a15cc667f23bfbbe201d42de08afa88a1567a7c6
madhuvayya <madhu.vayya@gmail.com>
2020-07-16 01:03:49 +0530

    Added code to get the user list

--- javascript.js
+++ javascript.js
@@ -1,3 +1,25 @@
+var userList = [
+    {
+        name:'Suresh',
+        email:'suresh@gmail.com',
+        status:'Active',
+        role:'Admin',
+        lastLogin:'3 Days ago',
+        permission:'Valid',
+        others:'...'
+    },
+
+    {
+        name:'Naresh',
+        email:'naresh@yahoo.com',
+        status:'Inactive',
+        role:'User',
+        lastLogin:'2 Days ago',
+        permission:'Invalid',
+        others:'...'
+    }
+]
+
 function showOptions(inputValue) {
     var x = document.getElementById(inputValue);
     if (x.style.display == "none") {
@@ -7,48 +29,25 @@ function showOptions(inputValue) {
     }
 }
 
-    var list1 = [];
-	var list2 = [];
-	var list3 = [];
-	var list4 = [];
-	var list5 = [];
-	var list6 = [];
+var html = "<table border='0'>";
 
-	var n = 0;
-	var x = 0;
+function createTable(){
+for (var i = 0; i < userList.length; i++) {
+    html+="<tr>";
+    html+="<td>"+'<img src="person-icon.png"/> '+userList[i].name+"</td>";
+    html+="<td>"+userList[i].email+"</td>";
+    html+="<td>"+userList[i].status+"</td>";
+    html+="<td>"+userList[i].role+"</td>";
+    html+="<td>"+userList[i].lastLogin+"</td>";
+    html+="<td>"+userList[i].permission+"</td>";
+    html+="<td>"+'...'+"</td>";
 
-function AddRow(){
+    html+="</tr>";
+}
+html+="</table>";
+}
 
-    var AddRown = document.getElementById('show');
-	var NewRow = AddRown.insertRow(0);
-
-			list1[x] = document.getElementById("name").value;
-			list2[x] = document.getElementById("email").value;
-			list3[x] = document.getElementById("status").value;
-			list4[x] = document.getElementById("role").value;
-			list5[x] = document.getElementById("lastlogin").value;
-            list6[x] = document.getElementById("permission").value;
-            
-            console.log(list3[x]);
-            window.alert(list3[x]);
-
-			var cel1 = NewRow.insertCell(0);
-			var cel2 = NewRow.insertCell(1);
-			var cel3 = NewRow.insertCell(2);
-            var cel4 = NewRow.insertCell(3);
-			var cel5 = NewRow.insertCell(4);
-			var cel6 = NewRow.insertCell(5);
-        
-
-			cel1.innerHTML = list1[x];
-			cel2.innerHTML = list2[x];
-			cel3.innerHTML = list3[x];
-			cel4.innerHTML = list4[x];
-			cel5.innerHTML = list5[x];
-            cel6.innerHTML = list6[x];
-            
-            console.log(cel1);
-
-			n++;
-			x++;
+function getData(){
+    createTable();
+    document.getElementById("box").innerHTML = html;
 }
