608031be10109f059a2c1dc82f9e361273e022e9
madhuvayya <madhu.vayya@gmail.com>
2020-07-16 11:19:40 +0530

    Added html code for user table heading

--- javascript.js
+++ javascript.js
@@ -2,20 +2,91 @@ var userList = [
     {
         name:'Suresh',
         email:'suresh@gmail.com',
-        status:'Active',
-        role:'Admin',
+        status:'<button class="active-button">Active</button>',
+        role:'Admnistrator',
         lastLogin:'3 Days ago',
         permission:'Valid',
         others:'...'
     },
-
     {
         name:'Naresh',
         email:'naresh@yahoo.com',
-        status:'Inactive',
+        status:'<button class="inactive-button">Inactive</button>',
         role:'User',
         lastLogin:'2 Days ago',
         permission:'Invalid',
+        others:'...'
+    },
+    {
+        name:'Phani',
+        email:'phani123@gmail.com',
+        status:'<button class="inactive-button">Inactive</button>',
+        role:'User',
+        lastLogin:'1 Days ago',
+        permission:'Invalid',
+        others:'...'
+    },
+    {
+        name:'Kalyan',
+        email:'kalyan.1994@yahoo.com',
+        status:'<button class="active-button">Active</button>',
+        role:'Admnistrator',
+        lastLogin:'1 Days ago',
+        permission:'Valid',
+        others:'...'
+    },
+    {
+        name:'Santhosh',
+        email:'dksanthosh@gmail.com',
+        status:'<button class="inactive-button">Inactive</button>',
+        role:'User',
+        lastLogin:'5 Days ago',
+        permission:'Invalid',
+        others:'...'
+    },
+    {
+        name:'Sathish P',
+        email:'patnam.sathish@gmail.com',
+        status:'<button class="active-button">Active</button>',
+        role:'Admnistrator',
+        lastLogin:'2 Days ago',
+        permission:'Valid',
+        others:'...'
+    },
+    {
+        name:'Mahesh',
+        email:'mahesh92@gmail.com',
+        status:'<button class="inactive-button">Inactive</button>',
+        role:'User',
+        lastLogin:'2 Days ago',
+        permission:'Invalid',
+        others:'...'
+    },
+    {
+        name:'jayanth',
+        email:'yjayanth@yahoo.com',
+        status:'<button class="active-button">Active</button>',
+        role:'Admnistrator',
+        lastLogin:'3 Days ago',
+        permission:'Valid',
+        others:'...'
+    },
+    {
+        name:'Trinath',
+        email:'trinath.123456@gmail.com',
+        status:'<button class="active-button">Active</button>',
+        role:'Admnistrator',
+        lastLogin:'1 Days ago',
+        permission:'Valid',
+        others:'...'
+    },
+    {
+        name:'Mithin',
+        email:'pmithinkumar@gmail.com',
+        status:'<button class="inactive-button">Inactive</button>',
+        role:'User',
+        lastLogin:'4 Days ago',
+        permission:'Inalid',
         others:'...'
     }
 ]
@@ -32,19 +103,19 @@ function showOptions(inputValue) {
 var html = "<table border='0'>";
 
 function createTable(){
-for (var i = 0; i < userList.length; i++) {
-    html+="<tr>";
-    html+="<td>"+'<img src="person-icon.png"/> '+userList[i].name+"</td>";
-    html+="<td>"+userList[i].email+"</td>";
-    html+="<td>"+userList[i].status+"</td>";
-    html+="<td>"+userList[i].role+"</td>";
-    html+="<td>"+userList[i].lastLogin+"</td>";
-    html+="<td>"+userList[i].permission+"</td>";
-    html+="<td>"+'...'+"</td>";
+    for (var i = 0; i < userList.length; i++) {
+        html+="<tr>";
+        html+="<td>"+'<img src="person-icon.png"/> '+ "&nbsp;"+ userList[i].name+"</td>";
+        html+="<td>"+userList[i].email+"</td>";
+        html+="<td>"+userList[i].status+"</td>";
+        html+="<td>"+userList[i].role+"</td>";
+        html+="<td>"+userList[i].lastLogin+"</td>";
+        html+="<td>"+userList[i].permission+"</td>";
+        html+="<td>"+'...'+"</td>";
 
-    html+="</tr>";
-}
-html+="</table>";
+        html+="</tr>";
+    }
+    html+="</table>";
 }
 
 function getData(){
